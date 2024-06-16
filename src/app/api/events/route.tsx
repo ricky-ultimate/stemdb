2a124e75f46d4563fddee5ba2bd913670f1b728a
Ricky Ultimate <rickyogigi@gmail.com>
2024-06-16 16:30:32 +0100

    Add default handler function to manage event API routes

--- src/app/api/events/route.tsx
+++ src/app/api/events/route.tsx
@@ -43,3 +43,25 @@ async function createEvent(req: NextApiRequest, res: NextApiResponse) {
     });
     res.status(204).end();
   }
+
+
+  export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    switch (req.method) {
+      case 'GET':
+        await getEvents(req, res);
+        break;
+      case 'POST':
+        await createEvent(req, res);
+        break;
+      case 'PUT':
+      case 'PATCH':
+        await updateEvent(req, res);
+        break;
+      case 'DELETE':
+        await deleteEvent(req, res);
+        break;
+      default:
+        res.setHeader('Allow', ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+  }
