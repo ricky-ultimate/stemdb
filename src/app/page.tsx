cc1cc1c8154fb0965e57dc3c011408d36e1556a3
David <davidiwuji@hotmail.com>
2024-06-16 14:39:18 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,7 +105,6 @@ export default function Home() {
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
             Instantly deploy your Next.js site to a shareable URL with Vercel.....
-
             
           </p>
         </a>
