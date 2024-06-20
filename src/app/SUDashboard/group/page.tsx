5de1b8b224ca9e17b46c65a8f1932710ffb2f21e
David <davidiwuji@hotmail.com>
2024-06-20 18:48:56 +0100

    made some changes (actuall a lot)

--- src/app/SUDashboard/group/page.tsx
+++ src/app/SUDashboard/group/page.tsx
@@ -1,23 +1,27 @@
+import Link from 'next/link';
+import Head from 'next/head';
 import { Metadata } from 'next';
-import MenuSU from '../components/SUMenu';
-import SUStatistics from '../components/SUStatistics';
+import Grouping from '../../components/grouping';
+import Menu from '@/app/components/UserMenu';
+import MenuSU from '@/app/components/SUMenu';
 
 
 export const metadata: Metadata = {
-    title: 'Dashboard SuperUser',
+    title: 'Group SuperUser',
   }
   
-export default function Users() {
+export default function Groups() {
     return (
         <>
             
             <div>
-                
+        {/* <Navbar/> */}
+        
         <div className="antialiased bg-black w-full min-h-screen text-slate-300 relative py-4">
- 
+  
       <div className="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
         <MenuSU/>
-        <SUStatistics/>
+        <Grouping/>
         
       </div>
     </div>
