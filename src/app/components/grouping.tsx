d12f80ac4645f4b6173e40a8d31bfb13f52591ca
David <davidiwuji@hotmail.com>
2024-06-20 20:16:57 +0100

    "Updated Grouping component with new UI elements and data, and modified Rank component with updated user information."

--- src/app/components/grouping.tsx
+++ src/app/components/grouping.tsx
@@ -9,9 +9,9 @@ const Grouping = () => {
         <div className="bg-white/10 col-span-9 rounded-lg p-6">
 
 
-            {/* Profile */}
+            {/* Group */}
             <div id="Group">
-                <h1 className="font-bold py-4 uppercase">Group #</h1>
+                <h1 className="font-bold py-4 uppercase">GUAP Group</h1>
                 <div id="stats" className="grid gird-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
 
                     <div className="flex bg-black shadow-lg rounded-lg overflow-hidden">
@@ -47,6 +47,159 @@ const Grouping = () => {
                     </div>
 
                     
+                </div>
+            </div>
+
+
+            <div id="last-users">
+                <h1 className="font-bold py-4 uppercase">Last 24h users</h1>
+                <div className="overflow-x-scroll">
+                    <table className="w-full whitespace-nowrap">
+                        <thead className="bg-black/60">
+                            <th className="text-left py-3 px-2 rounded-l-lg">Name</th>
+                            <th className="text-left py-3 px-2">Email</th>
+                            <th className="text-left py-3 px-2">Group</th>
+                            <th className="text-left py-3 px-2">Status</th>
+                            <th className="text-left py-3 px-2 rounded-r-lg">Actions</th>
+                        </thead>
+                        <tr className="border-b border-gray-700">
+                            <td className="py-3 px-2 font-bold">
+                                <div className="inline-flex space-x-3 items-center">
+                                    <span><img className="rounded-full w-8 h-8" src="https://images.generated.photos/tGiLEDiAbS6NdHAXAjCfpKoW05x2nq70NGmxjxzT5aU/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTM4ODM1LmpwZw.jpg" alt=""/></span>
+                                    <span>Thai Mei</span>
+                                </div>
+                            </td>
+                            <td className="py-3 px-2">thai.mei@abc.com</td>
+                            <td className="py-3 px-2">User</td>
+                            <td className="py-3 px-2">Approved</td>
+                            <td className="py-3 px-2">
+                                <div className="inline-flex items-center space-x-3">
+                                    <a href="" title="Edit" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                      </svg>
+                                      </a>
+                                    <a href="" title="Edit password" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                                      </svg>
+                                      </a>
+                                    <a href="" title="Suspend user" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                                      </svg>
+                                      </a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr className="border-b border-gray-700">
+                            <td className="py-3 px-2 font-bold">
+                                <div className="inline-flex space-x-3 items-center">
+                                    <span><img className="rounded-full w-8 h-8" src="https://images.generated.photos/tGiLEDiAbS6NdHAXAjCfpKoW05x2nq70NGmxjxzT5aU/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTM4ODM1LmpwZw.jpg" alt=""/></span>
+                                    <span>Thai Mei</span>
+                                </div>
+                            </td>
+                            <td className="py-3 px-2">thai.mei@abc.com</td>
+                            <td className="py-3 px-2">User</td>
+                            <td className="py-3 px-2">Approved</td>
+                            <td className="py-3 px-2">
+                                <div className="inline-flex items-center space-x-3">
+                                    <a href="" title="Edit" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                      </svg>
+                                      </a>
+                                    <a href="" title="Edit password" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                                      </svg>
+                                      </a>
+                                    <a href="" title="Suspend user" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                                      </svg>
+                                      </a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr className="border-b border-gray-700">
+                            <td className="py-3 px-2 font-bold">
+                                <div className="inline-flex space-x-3 items-center">
+                                    <span><img className="rounded-full w-8 h-8" src="https://images.generated.photos/tGiLEDiAbS6NdHAXAjCfpKoW05x2nq70NGmxjxzT5aU/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTM4ODM1LmpwZw.jpg" alt=""/></span>
+                                    <span>Thai Mei</span>
+                                </div>
+                            </td>
+                            <td className="py-3 px-2">thai.mei@abc.com</td>
+                            <td className="py-3 px-2">User</td>
+                            <td className="py-3 px-2">Approved</td>
+                            <td className="py-3 px-2">
+                                <div className="inline-flex items-center space-x-3">
+                                    <a href="" title="Edit" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                      </svg>
+                                      </a>
+                                    <a href="" title="Edit password" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                                      </svg>
+                                      </a>
+                                    <a href="" title="Suspend user" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                                      </svg>
+                                      </a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr className="border-b border-gray-700">
+                            <td className="py-3 px-2 font-bold">
+                                <div className="inline-flex space-x-3 items-center">
+                                    <span><img className="rounded-full w-8 h-8" src="https://images.generated.photos/niCVbIBAm4hahzwS83HoEtcVEIactkKohOzgXWYY4lM/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTk4ODczLmpwZw.jpg" alt=""/></span>
+                                    <span>Marquez Spineli</span>
+                                </div>
+                            </td>
+                            <td className="py-3 px-2">marquez.spineli@cba.com</td>
+                            <td className="py-3 px-2">User</td>
+                            <td className="py-3 px-2">Approved</td>
+                            <td className="py-3 px-2">
+                                <div className="inline-flex items-center space-x-3">
+                                    <a href="" title="Edit" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                      </svg>
+                                      </a>
+                                    <a href="" title="Edit password" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                                      </svg>
+                                      </a>
+                                    <a href="" title="Suspend user" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                                      </svg>
+                                      </a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr className="border-b border-gray-800">
+                            <td className="py-3 px-2 font-bold">
+                                <div className="inline-flex space-x-3 items-center">
+                                    <span><img className="rounded-full w-8 h-8" src="https://images.generated.photos/f_xU7q780YXiKG7IwKVV05eU6Sj2nIodEkN1S8GyM2M/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NDk2MTc4LmpwZw.jpg" alt=""/></span>
+                                    <span>Mark Spike</span>
+                                </div>
+                            </td>
+                            <td className="py-3 px-2">mark.spike@abc.com</td>
+                            <td className="py-3 px-2">Administrator</td>
+                            <td className="py-3 px-2">Approved</td>
+                            <td className="py-3 px-2">
+                                <div className="inline-flex items-center space-x-3">
+                                    <a href="" title="Edit" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                      </svg>
+                                      </a>
+                                    <a href="" title="Edit password" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                                      </svg>
+                                      </a>
+                                    <a href="" title="Suspend user" className="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                                      </svg>
+                                      </a>
+                                </div>
+                            </td>
+                        </tr>
+
+                        
+                    </table>
                 </div>
             </div>
 
--- src/app/components/rank.tsx
+++ src/app/components/rank.tsx
@@ -229,8 +229,8 @@ const Ranks = () => {
                         <div className="flex bg-black shadow-lg rounded-lg overflow-hidden relative">
                             <img src="/static/user1.jpg" className="object-cover w-32 h-32 rounded-l-lg" alt="Profile Picture" />
                             <div className="flex flex-col justify-center ml-4 flex-grow">
-                                <h2 className="text-xl font-bold text-white">Space Dad</h2>
-                                <p className="text-gray-500 font-medium">Cyber Security</p>
+                                <h2 className="text-xl font-bold text-white">Celestine Okonkwo</h2>
+                                <p className="text-gray-500 font-medium">Comp. Science</p>
                                 <p className="text-gray-500 text-sm">200 Level</p>
                             </div>
                             <div className="m-8">
