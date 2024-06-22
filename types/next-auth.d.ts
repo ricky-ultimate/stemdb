3b801166a83ebc085802fa84d6bd011047a2f64b
Ricky Ultimate <rickyogigi@gmail.com>
2024-06-22 08:52:29 +0100

    Update TypeScript declarations for NextAuth

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -19,3 +19,13 @@ declare module 'next-auth' {
     image?: string | null;
   }
 }
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id: string;
+    role: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  }
+}
