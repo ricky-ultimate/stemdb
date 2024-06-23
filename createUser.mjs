bc966b94640bfcc941a18e1a2052a299b355afd2
David <davidiwuji@hotmail.com>
2024-06-23 23:30:55 +0100

    "Updated createUser.mjs with new user data and modified SignIn.tsx with changes to layout and styling"

--- createUser.mjs
+++ createUser.mjs
@@ -9,10 +9,10 @@ async function main() {
 
   const user = await prisma.member.create({
     data: {
-      firstName: 'New',
-      lastName: 'User',
-      matricno: '1234',
-      email: 'new@.com',
+      firstName: 'space',
+      lastName: 'dad',
+      matricno: '12345678',
+      email: 'spacedad@milky.com',
       password: hashedPassword, // Store hashed password
       role: 'USER',
     },
