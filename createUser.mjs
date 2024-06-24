2d983018e9c3f8983234cb744d9e189a2359806f
Ricky Ultimate <rickyogigi@gmail.com>
2024-06-25 00:05:20 +0100

    chore(createUser): Update createUser.mjs with new user data

--- createUser.mjs
+++ createUser.mjs
@@ -13,10 +13,8 @@ async function main() {
       lastName: 'dad',
       matricno: '12345678',
       email: 'spacedad@milky.com',
- 
       password: hashedPassword, // Store hashed password
       role: 'USER',
->>>>>>> e39386a ("Update createUser function: changed matricno to '1234', email to 'new@.com', and role to 'USER'")
     },
   });
 
