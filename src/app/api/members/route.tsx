6e73e748a38419bb8b2d587d2eae018eb28dc7ce
Ricky Ultimate <rickyogigi@gmail.com>
2024-06-25 23:50:31 +0100

    feat: Update createMember function to hash passwords

--- src/app/api/members/route.tsx
+++ src/app/api/members/route.tsx
@@ -3,26 +3,27 @@ import prisma from "../../../../prisma/prisma/prisma";
 import bcrypt from 'bcryptjs';
 
 async function createMember(req: NextApiRequest, res: NextApiResponse) {
-    const { firstName, lastName, matricno, email, role } = req.body;
-    if (!firstName || !lastName || !matricno || !email || !role) {
-      return res.status(400).json({ error: 'All fields are required' });
-    }
-    try {
-      const member = await prisma.member.create({
-        data: {
-          firstName,
-          lastName,
-          matricno,
-          email,
-          role,
-          password: '123456789',
-        },
-      });
-      res.status(201).json(member);
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to create member' });
-    }
+  const { firstName, lastName, matricno, email, role, password } = req.body;
+  if (!firstName || !lastName || !matricno || !email || !role || !password) {
+    return res.status(400).json({ error: 'All fields are required' });
   }
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const member = await prisma.member.create({
+      data: {
+        firstName,
+        lastName,
+        matricno,
+        email,
+        role,
+        password: hashedPassword,
+      },
+    });
+    res.status(201).json(member);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create member' });
+  }
+}
 
   async function getMembers(req: NextApiRequest, res: NextApiResponse) {
     try {
