bf34fcb3d872a3db30e89bd8e92c6f1906769958
Ricky Ultimate <rickyogigi@gmail.com>
2024-08-02 14:46:44 +0100

    feat(API): Update GET function in projects API to include members

--- src/app/api/projects/route.tsx
+++ src/app/api/projects/route.tsx
@@ -30,7 +30,11 @@ export async function POST(req: Request) {
 
 export async function GET() {
   try {
-    const projects = await prisma.project.findMany();
+    const projects = await prisma.project.findMany({
+      include: {
+        members: true,
+      },
+    });
     return NextResponse.json(projects, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: 'Failed to fetch projects' }, { status: 500 });
