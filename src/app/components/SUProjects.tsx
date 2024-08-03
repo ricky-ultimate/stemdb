fe78c0b0f9058588e906e4edcf92da11e7f815b2
Ricky Ultimate <rickyogigi@gmail.com>
2024-08-03 03:12:37 +0100

    Fix members field handling in Projects component

--- src/app/components/SUProjects.tsx
+++ src/app/components/SUProjects.tsx
@@ -145,8 +145,8 @@ const Projects = () => {
                 isMulti
                 options={members.map(member => ({ value: member.id, label: `${member.firstName} ${member.lastName}` }))}
                 value={editProject.members?.map(member => ({
-                  value: member.id,
-                  label: `${member.firstName} ${member.lastName}`
+                  value: member?.id,
+                  label: `${member?.firstName} ${member?.lastName}`
                 }))}
                 onChange={(selectedOptions) => handleMembersChange(selectedOptions, true)}
               />
@@ -155,7 +155,7 @@ const Projects = () => {
               <button onClick={() => saveProjectRow(project.id)} className="px-2 py-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="hover:text-green-500 w-5 h-5 bi bi-floppy" viewBox="0 0 16 16">
                   <path d="M11 2H9v3h2z" />
-                  <path d="M1.5 0h11.586a1.5.5 0 0 1 1.06.44l1.415 1.414A1.5.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5.5 0 0 1 0 14.5v-13A1.5.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5m3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4zM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5z" />
+                  <path d="M1.5 0h11.586a1.5.5 0 0 1 1.06.44l1.415 1.414A1.5.5 0 0 1 16 2.914V14.5a1.5.5 0 0 1-1.5 1.5h-13A1.5.5 0 0 1 0 14.5v-13A1.5.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5m3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4zM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5z" />
                 </svg>
               </button>
             </td>
@@ -168,7 +168,7 @@ const Projects = () => {
             <td className="py-3 px-2">{new Date(project.startDate).toLocaleDateString()}</td>
             <td className="py-3 px-2">{project.endDate ? new Date(project.endDate).toLocaleDateString() : 'N/A'}</td>
             <td className="py-3 px-2">
-              {project.members.map(member => `${member.firstName} ${member.lastName}`).join(', ')}
+              {project.members?.map(member => `${member.firstName} ${member.lastName}`).join(', ')}
             </td>
             <td className="py-3 px-2 text-right">
               <button onClick={() => editProjectRow(project)} className="px-2 py-1">
@@ -241,10 +241,10 @@ const Projects = () => {
           <Select
             isMulti
             options={members.map(member => ({ value: member.id, label: `${member.firstName} ${member.lastName}` }))}
-            value={newProject.members?.map(memberId => {
-              const member = members.find(member => member.id === memberId);
-              return member ? { value: member.id, label: `${member.firstName} ${member.lastName}` } : null;
-            })}
+            value={newProject.members?.map(member => ({
+              value: member.id,
+              label: `${member.firstName} ${member.lastName}`
+            }))}
             onChange={(selectedOptions) => handleMembersChange(selectedOptions, false)}
             className="text-black"
           />
