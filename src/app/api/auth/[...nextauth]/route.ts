58193f04432ab2a19d139613fa986738aac11fc4
Ricky Ultimate <rickyogigi@gmail.com>
2025-01-23 10:20:29 +0100

    chore: fix the attrocity I made

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '../../../prisma/prisma/prisma';
+import prisma from '../../../../../prisma/prisma/prisma';
 import bcrypt from 'bcryptjs';
 
 export default NextAuth({
